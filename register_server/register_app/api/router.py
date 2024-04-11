f64d88115b2238c28f8a21c863968f928103aa66
hyeon <selwha1@naver.com>
2024-04-11 19:53:19 +0900

    add router users,init-db

---
+++ register_server/register_app/api/router.py
@@ -0,0 +1,7 @@
+from fastapi import APIRouter
+
+from register_app.api.endpoints import users, init_db
+
+api_router = APIRouter()
+api_router.include_router(users.router, prefix="/users", tags=["users"])
+api_router.include_router(init_db.router, prefix="/init-db", tags=["init"])
