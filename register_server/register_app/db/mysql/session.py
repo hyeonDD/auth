448a53003a56416e9e38812a9010ce61bd5b46c8
hyeon <selwha1@naver.com>
2024-04-11 19:52:35 +0900

    add mysql session

---
+++ register_server/register_app/db/mysql/session.py
@@ -0,0 +1,11 @@
+from register_app.core.config import settings
+
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.ext.asyncio import async_sessionmaker
+
+# echo bool 값에 따라 실행하는 쿼리 확인 가능
+engine = create_async_engine(settings.MYSQL_URL, echo=True)
+
+# expire_on_commit bool 값으로 커밋후 객체 만료 설정 가능
+mysql_session_factory = async_sessionmaker(
+    engine, expire_on_commit=True, class_=AsyncSession)
