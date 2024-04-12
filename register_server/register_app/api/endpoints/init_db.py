9e0273f786152e42491769f7738484801e21a90a
hyeon <selwha1@naver.com>
2024-04-12 23:32:57 +0900

    add supueruser init

--- register_server/register_app/api/endpoints/init_db.py
+++ register_server/register_app/api/endpoints/init_db.py
@@ -1,8 +1,10 @@
+from fastapi import APIRouter
+from sqlalchemy import insert
+
 from register_app.db.mysql.base_class import Base
 from register_app.db.mysql.session import engine
-
-from fastapi import APIRouter
-
+from register_app.models.mysql.user import User
+from register_app.core.security import get_password_hash
 
 router = APIRouter()
 
@@ -11,7 +13,7 @@ router = APIRouter()
 async def create_tables() -> None:
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
-
+        await conn.execute(insert(User).values(nickname='superuser', email='superuser@example.com', password=get_password_hash('1'), permission='superuser'))
     return {"message": "Table and Colum Create Successful"}
 
 
