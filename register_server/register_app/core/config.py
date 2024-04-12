fff9c5dcf8a34b72ae5777fb9369621bb29f9903
hyeon <selwha1@naver.com>
2024-04-12 23:35:57 +0900

    add redis url, superuser header

--- register_server/register_app/core/config.py
+++ register_server/register_app/core/config.py
@@ -3,7 +3,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     MYSQL_URL: str
+    REDIS_URL: str
     PREFIX_URL: str
+    SUPERUSER_HEADER: str
 
     model_config = SettingsConfigDict(
         env_file='.env', env_file_encoding='utf-8')
