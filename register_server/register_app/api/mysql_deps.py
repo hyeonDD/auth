1dfe5eda27e8aa4c88fa0683c0677fa8422d4524
hyeon <selwha1@naver.com>
2024-04-12 23:34:06 +0900

    rename function name

--- register_server/register_app/api/mysql_deps.py
+++ register_server/register_app/api/mysql_deps.py
@@ -3,6 +3,6 @@ from typing import AsyncGenerator
 from register_app.db.mysql.session import mysql_session_factory
 
 
-async def get_db() -> AsyncGenerator:
+async def get_mysql_db() -> AsyncGenerator:
     async with mysql_session_factory() as db:
         yield db
