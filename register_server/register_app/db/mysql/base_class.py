cfa3a6df025b9bfd5e023c3f1015d438aaa63604
hyeon <selwha1@naver.com>
2024-04-13 01:24:46 +0900

    replace column type

--- register_server/register_app/db/mysql/base_class.py
+++ register_server/register_app/db/mysql/base_class.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import as_declarative, declared_attr
 # python class를 사용하여 sqlalchemy의 orm을 수행할 수 있도록 wrapping
 @as_declarative()
 class Base:
-    id: int
+    id: str
     __name__: str
 
     # class 이름을 소문자로 __tablename__ 속성 만들기
--- register_server/register_app/models/mysql/user.py
+++ register_server/register_app/models/mysql/user.py
@@ -1,10 +1,12 @@
-from sqlalchemy import Column, Integer, String, TIMESTAMP, func
+from uuid import uuid4
+from sqlalchemy import Column, String, TIMESTAMP, func
 
 from register_app.db.mysql.base_class import Base
 
 
 class User(Base):
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(String(36), default=lambda: uuid4(),
+                primary_key=True)
     nickname = Column(String(20), unique=True, nullable=False)
     email = Column(String(255), unique=True, index=True, nullable=False)
     password = Column(String(100), nullable=False)
