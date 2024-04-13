d71c49924038ef28eae161011c794755eb29a726
hyeon <selwha1@naver.com>
2024-04-13 20:09:57 +0900

    test users enpoint test

---
+++ register_server/register_app/tests/api/test_users.py
@@ -0,0 +1,55 @@
+import pytest
+from random import randint
+from httpx import AsyncClient
+
+from register_app.main import app
+from register_app.core.config import settings
+
+rand_num = randint(1, 9999)
+
+test_data = {
+    "email": f"assert{rand_num}@example.com",
+    "password": "1",
+    "nickname": f"assert{rand_num}",
+    "permission": "user"
+}
+
+base_url = "http://localhost:8000"
+
+
+@pytest.mark.asyncio
+async def test_root():
+    async with AsyncClient(app=app, base_url=f"{base_url}") as ac:
+        response = await ac.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "This is register server!!"}
+
+
+@pytest.mark.asyncio
+async def test_users_without_header():
+    """
+    헤더 없이 post
+    """
+    async with AsyncClient(app=app, base_url=f"{base_url}{settings.PREFIX_URL}/users") as ac:
+        response = await ac.post("/", json=test_data)
+    assert response.status_code == 401 or 409
+    assert response.json() == {
+        "detail": {
+            "error": "Unauthorized user",
+            "message": "You do not have permission"
+        }
+    } or {"detail": {
+        "error": "User already exists",
+        "message": "The user with the provided nickname or email already exists."
+    }}
+
+
+@pytest.mark.asyncio
+async def test_users_with_header():
+    """
+    헤더 있이 post
+    """
+    async with AsyncClient(app=app, base_url=f"{base_url}{settings.PREFIX_URL}/users", headers={'SUPERUSER-HEADER': settings.SUPERUSER_HEADER}) as ac:
+        response = await ac.post("/", json=test_data)
+    assert response.status_code == 201
+    assert response.json() == {"message": "User Create Successful"}
