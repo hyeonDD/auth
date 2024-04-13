e123afb5b4dd282d7ab3820a146c9510a504ccbb
hyeon <selwha1@naver.com>
2024-04-13 20:10:18 +0900

    fix type hint dict -> str

--- register_server/register_app/crud/redis/crud_user.py
+++ register_server/register_app/crud/redis/crud_user.py
@@ -1,7 +1,7 @@
 import redis.asyncio as redis
 
 
-async def create_cache(db: redis.Redis, *, key: str, value: dict) -> None:
+async def create_cache(db: redis.Redis, *, key: str, value: str) -> None:
     """
     redis cache 만들기
     """
