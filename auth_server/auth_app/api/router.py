7ef4e9b0d76d78753dd0b8cc3cf6c9c43305f60c
hyeon <selwha1@naver.com>
2024-04-18 22:17:18 +0900

    feat logout router

--- auth_server/auth_app/api/router.py
+++ auth_server/auth_app/api/router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
-from auth_app.api.endpoints import login
+from auth_app.api.endpoints import login, logout
 
 api_router = APIRouter()
 api_router.include_router(login.router, prefix="/login", tags=["tokens"])
+api_router.include_router(logout.router, prefix="/logout", tags=["tokens"])
