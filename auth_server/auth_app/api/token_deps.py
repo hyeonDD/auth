8f90c7e2efb85b8e5e73ebeb75c95d1b64e83835
hyeon <selwha1@naver.com>
2024-04-18 22:16:34 +0900

    fix access token check

--- auth_server/auth_app/api/token_deps.py
+++ auth_server/auth_app/api/token_deps.py
@@ -1,8 +1,10 @@
-from typing import Annotated
-from fastapi import Cookie, HTTPException, status
+from fastapi import HTTPException, status, Request
 
 
-def exist_access_token(access_token: Annotated[str, Cookie()]):
+def exist_access_token(request: Request):
     """
     access token 유/무 확인
     """
+    if not request.cookies.get('access_token'):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Access token not found in cookie")
